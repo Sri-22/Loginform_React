8c3d97b729391bb0e7ff9f7b7d010699c52df1f9
Sri-22 <sri.pasupuleti22@gmail.com>
2023-08-25 19:17:11 +0530

    Concepts - Used : onClick , onChange , onSubmit , useState - React Hook , object destructuring.

---
+++ src/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Display extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+            {this.props.name}
+        </h2>
+      </div>
+    )
+  }
+}
